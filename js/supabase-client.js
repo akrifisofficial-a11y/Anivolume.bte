6f5cd9f4d6a3b33413b7402a3747d10e64e5d8c1
akrifisofficial-a11y <akrifisofficial@gmail.com>
2026-07-06 00:24:33 +0600

    Update supabase-client.js

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-const SUPABASE_URL = 'https://ваш-проект.supabase.co';
-const SUPABASE_ANON_KEY = 'ваш-anon-ключ';
+const SUPABASE_URL = 'https://hiehzjvhtnibkemrvarj.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpZWh6anZodG5pYmtlbXJ2YXJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMyNjc3OTEsImV4cCI6MjA5ODg0Mzc5MX0.9d28QFEjvEZ8uxFRPgPIm6Ig1_vJTN4WcWL4mPwnHP4';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
